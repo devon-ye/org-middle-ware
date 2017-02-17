d9c8446f8adb41711ad6dcdd1e68cc166a629c4d
lenovo <lenovo@DESKTOP-8EPBEP6>
2017-02-18 01:44:53 +0800

    [init]

---
+++ kafka-demo/src/main/java/org/kafka/async/demo/kafkaAsyncProducer.java
@@ -0,0 +1,11 @@
+package org.kafka.async.demo;
+/**
+*Describetion:
+*@author  Devonmusa
+*@version
+*2017年2月18日 上午1:43:32
+*
+*/
+public class kafkaAsyncProducer {
+
+}
---
+++ kafka-demo/src/main/java/org/kafka/demo/kafkaProducer.java
@@ -0,0 +1,11 @@
+package org.kafka.demo;
+/**
+*Describetion:
+*@author  Devonmusa
+*@version
+*2017年2月18日 上午1:42:04
+*
+*/
+public class kafkaProducer {
+
+}
