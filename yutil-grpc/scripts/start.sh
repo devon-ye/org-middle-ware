0d3366a70a53bc01a1024317c73912da9f70991b
yedewen <yedewen@boco.com.cn>
2017-07-31 01:56:12 +0800

    [update] refactory project name

---
+++ yutil-grpc/scripts/start.sh
@@ -0,0 +1,13 @@
+#!/usr/sh
+
+
+#===========================================================#
+#                                                           #
+#                                                           #
+#     script  start grpc server demo                        #
+#                                                           #
+#                                                           #
+#===========================================================#
+
+
+java   org.yutil.grpc.service.DemoServiceServerTest
