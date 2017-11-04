6cffffa5a918dd15a8b435a95422c65d5456c5ce
yedewen <yedewen@boco.com.cn>
2017-11-04 16:52:44 +0800

    update pom

--- yutil-infinispan/src/main/java/org/infinispan/app/InfinispanApp.java
+++ yutil-infinispan/src/main/java/org/infinispan/app/InfinispanApp.java
@@ -4,13 +4,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
 import org.apache.log4j.PropertyConfigurator;
 import org.infinispan.cache.cluster.jgroup.JgroupConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.joran.JoranConfigurator;
+
 
 /**
  * @see
@@ -56,22 +58,31 @@ public class InfinispanApp {
 
 	private static void initLogback(String appHome) throws Exception {
 		logger.info("init logback");
-		String logBackConfig = appHome + "/config/logback.xml";
-		File logBackConfigFile = new File(logBackConfig);
-		if (logBackConfigFile.exists()) {
-			LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-			JoranConfigurator configurator = new JoranConfigurator();
-			configurator.setContext(loggerContext);
-			loggerContext.reset();
-			configurator.doConfigure(logBackConfig);
+		String logBackConfigPath = appHome + "/config/logback.xml";
+		File logBackConfigFile = new File(logBackConfigPath);
+		if (!logBackConfigFile.exists()) {
+            logger.error("log4j config file not exists ,it configFilePath=" + logBackConfigPath);
+            System.err.println("log4j config file not exists ,it configFilePath=" + logBackConfigPath);
+            return;
 		}
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(loggerContext);
+        loggerContext.reset();
+        configurator.doConfigure(logBackConfigPath);
 	}
 
 	private static void initLog4j(String appHome) throws Exception {
 		logger.info("init log4j");
-		String log4jConfig = appHome + "/config/log4j.properties";
+		String log4jConfigPath = appHome + "/config/log4j.properties";
+        File log4jConfigFile = new File(log4jConfigPath);
+        if(!log4jConfigFile.exists()){
+            logger.error("log4j config file not exists ,it configFilePath=" + log4jConfigPath);
+            System.err.println("log4j config file not exists ,it configFilePath=" + log4jConfigPath);
+            return;
+        }
 		Properties log4jProperties = new Properties();
-		log4jProperties.load(new FileInputStream(log4jConfig));
+		log4jProperties.load(new FileInputStream(log4jConfigPath));
 		PropertyConfigurator.configure(log4jProperties);
 	}
 }
