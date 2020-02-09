87e7222d38f14a0f5f46d9992de9b8b0f1021d49
dewen.ye <dewen.ye@qunar.com>
2020-02-09 19:37:32 +0800

    update sql

--- mw-iam/iam-server/sql/mysql-init.sql
+++ mw-iam/iam-server/sql/mysql-init.sql
@@ -1,36 +1,56 @@
-create databases iamdb if exists;
+/*
+ create database
+ */
+DROP DATABASE IF EXISTS iam_db;
+CREATE DATABASE iam_db;
 
-use iamdb;
-DROP IF EXISTS user;
-CREATE TABLE user(
-  id int(32) NOT NULL UNIQUE ;
-  name varchar(128);
-  passwd varchar(256);
-);
+USE iam_db;
 
-DROP IF EXISTS group;
-CREATE TABLE group(
+/*-----------init all table-----------------------------*/
+DROP TABLE IF EXISTS user;
+CREATE TABLE user
+(
+    id       int(32) NOT NULL UNIQUE,
+    name     varchar(128),
+    password varchar(256),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-);
-
-
-DROP IF EXISTS resource;
-CREATE TABLE resource(
-
-);
-
-
-DROP IF EXISTS policy;
-CREATE TABLE policy(
-
-);
-
-DROP IF EXISTS permition;
-CREATE TABLE permition(
-
-);
+DROP TABLE IF EXISTS group_info;
+CREATE TABLE group_
+(
+    id   int(32) NOT NULL UNIQUE,
+    name varchar(128),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
+DROP TABLE IF EXISTS resource;
+CREATE TABLE resource
+(
+    id   int(32) NOT NULL UNIQUE,
+    name varchar(128),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+DROP TABLE IF EXISTS policy;
+CREATE TABLE policy
+(
+    id   int(32) NOT NULL UNIQUE,
+    name varchar(128),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
-
+DROP TABLE IF EXISTS action;
+CREATE TABLE action
+(
+    id   int(32) NOT NULL UNIQUE,
+    name varchar(128),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
