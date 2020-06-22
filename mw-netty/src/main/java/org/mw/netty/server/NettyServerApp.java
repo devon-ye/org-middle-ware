1d5014a1246888465000b0bb8e85b473a3b0b20a
dewen.ye <dewen.ye@qunar.com>
2020-06-23 01:15:03 +0800

    [update]  dependency config

--- mw-netty/src/main/java/org/mw/netty/server/NettyServerApp.java
+++ mw-netty/src/main/java/org/mw/netty/server/NettyServerApp.java
@@ -11,6 +11,8 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author devon.ye
@@ -18,49 +20,49 @@ import io.netty.handler.logging.LoggingHandler;
  * @since
  */
 public class NettyServerApp {
-
-	private static final String OS_NAME = "os.name";
-	private static final String LINUX = "Linux";
-
-
-	public static void main(String[] args) {
-		ServerBootstrap serverBootstrap = new ServerBootstrap();
+    private static  Logger LOG = LoggerFactory.getLogger(NettyServerApp.class);
+    private static final String OS_NAME = "os.name";
+    private static final String LINUX = "Linux";
 
 
-		System.out.println(System.getProperties());
-		EventLoopGroup bossEventLoopGroup = null;
-		EventLoopGroup workEventLoopGroup = null;
-		if (LINUX.equals(System.getProperty(OS_NAME))) {
-			bossEventLoopGroup = new EpollEventLoopGroup();
-			workEventLoopGroup = new EpollEventLoopGroup();
-		} else {
-			bossEventLoopGroup = new NioEventLoopGroup();
-			workEventLoopGroup = new NioEventLoopGroup();
-		}
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        LOG.info("properties:{}",System.getProperties());
+        String osName = System.getProperty(OS_NAME);
+        LOG.info("osName:{}", osName);
+        EventLoopGroup bossEventLoopGroup = null;
+        EventLoopGroup workEventLoopGroup = null;
+        if (LINUX.equals(osName)) {
+            bossEventLoopGroup = new EpollEventLoopGroup();
+            workEventLoopGroup = new EpollEventLoopGroup();
+        } else {
+            bossEventLoopGroup = new NioEventLoopGroup();
+            workEventLoopGroup = new NioEventLoopGroup();
+        }
 
-		serverBootstrap.group(bossEventLoopGroup, workEventLoopGroup)
-				.channel(NioServerSocketChannel.class)
-				.handler(new LoggingHandler())
-				.childHandler(new ChannelInitializer<SocketChannel>() {
-					@Override
-					protected void initChannel(SocketChannel ch) throws Exception {
-						ChannelPipeline pipeline = ch.pipeline();
-						pipeline.addLast(new LoggingHandler(LogLevel.DEBUG));
-					}
-				});
-		/**
-		 * socket
-		 */
-		ChannelFuture channelFuture = null;
-		try {
-			channelFuture = serverBootstrap.bind(18081).sync();
-			channelFuture.channel().closeFuture().sync();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			bossEventLoopGroup.shutdownGracefully();
-			workEventLoopGroup.shutdownGracefully();
-		}
+        serverBootstrap.group(bossEventLoopGroup, workEventLoopGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler())
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new LoggingHandler(LogLevel.DEBUG));
+                    }
+                });
+        /**
+         * socket
+         */
+        ChannelFuture channelFuture = null;
+        try {
+            channelFuture = serverBootstrap.bind(18081).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossEventLoopGroup.shutdownGracefully();
+            workEventLoopGroup.shutdownGracefully();
+        }
 
-	}
+    }
 }
